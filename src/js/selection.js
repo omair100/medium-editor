916776d8a8fe6b29eb0ade233e7d9ddb2dfd0f1a
Omair Qadir <omair@inception.io>
2018-02-26 14:13:01 +0000

    Fix issue with selection in shadowdom

--- src/js/selection.js
+++ src/js/selection.js
@@ -339,7 +339,7 @@
             // Walk over block elements, counting number of empty blocks between last piece of text
             // and the block the cursor is in
             var closestBlock = MediumEditor.util.getClosestBlockContainer(cursorContainer),
-                treeWalker = doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, filterOnlyParentElements, false),
+                treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, filterOnlyParentElements, false),
                 emptyBlocksCount = 0;
             while (treeWalker.nextNode()) {
                 var blockIsEmpty = treeWalker.currentNode.textContent === '';
